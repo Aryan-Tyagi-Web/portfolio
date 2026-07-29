6bd1df4c3b406c6b803caa4be7791e992625efab
Aryan Tyagi <tyagiaryannda@gmail.com>
2026-07-29 20:22:55 +0530

    scroll

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Aryan Portfolio Loaded");
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+};
+
+window.addEventListener("beforeunload", () => {
+    window.scrollTo(0, 0);
+});
