f66ce2757116448dc877063e5a3a02e07c87bdc6
Aryan Tyagi <tyagiaryannda@gmail.com>
2026-07-29 21:52:24 +0530

    menu

--- script.js
+++ script.js
@@ -13,3 +13,12 @@ const navMenu = document.querySelector("nav ul");
 menuToggle.addEventListener("click", () => {
     navMenu.classList.toggle("active");
 });
+
+
+const navLinks = document.querySelectorAll("nav ul a");
+
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        navMenu.classList.remove("active");
+    });
+});
