1f7b9c387ec180cf85bd18ea2f786f0aa03e6610
Aryan Tyagi <tyagiaryannda@gmail.com>
2026-07-30 21:38:27 +0530

    scroll progress

--- script.js
+++ script.js
@@ -106,3 +106,17 @@ menuLinks.forEach(link => {
     });
 
 });
+
+const progressBar = document.querySelector(".scroll-progress");
+
+window.addEventListener("scroll", () => {
+
+    const scrollTop = window.scrollY;
+
+    const docHeight = document.documentElement.scrollHeight - window.innerHeight;
+
+    const progress = (scrollTop / docHeight) * 100;
+
+    progressBar.style.width = progress + "%";
+
+});
