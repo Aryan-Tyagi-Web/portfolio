4876710c0e7dd9360293f79de35b3db8eb980dbf
Aryan Tyagi <tyagiaryannda@gmail.com>
2026-07-31 19:52:23 +0530

    cursor

--- cursor.js
+++ cursor.js
@@ -11,35 +11,35 @@ if(!isTouch){
     const dot = document.querySelector(".cursor-dot");
     const ring = document.querySelector(".cursor-ring");
 
-    let mouseX = window.innerWidth/2;
-    let mouseY = window.innerHeight/2;
+    let mouseX = 0;
+let mouseY = 0;
 
-    let ringX = mouseX;
-    let ringY = mouseY;
+let ringX = 0;
+let ringY = 0;
 
-    document.addEventListener("mousemove",(e)=>{
+document.addEventListener("mousemove",(e)=>{
 
-        mouseX = e.clientX;
-        mouseY = e.clientY;
+    mouseX = e.clientX;
+    mouseY = e.clientY;
 
-        dot.style.left = mouseX+"px";
-        dot.style.top  = mouseY+"px";
+});
 
-    });
+function animate(){
 
-    function animateCursor(){
+    ringX += (mouseX-ringX)*0.15;
+    ringY += (mouseY-ringY)*0.15;
 
-        ringX += (mouseX-ringX)*0.18;
-        ringY += (mouseY-ringY)*0.18;
+    document.querySelector(".cursor-dot").style.transform =
+    `translate(${mouseX}px,${mouseY}px) translate(-50%,-50%)`;
 
-        ring.style.left = ringX+"px";
-        ring.style.top  = ringY+"px";
+    document.querySelector(".cursor-ring").style.transform =
+    `translate(${ringX}px,${ringY}px) translate(-50%,-50%)`;
 
-        requestAnimationFrame(animateCursor);
+    requestAnimationFrame(animate);
 
-    }
+}
 
-    animateCursor();
+animate();
 
     const hoverItems = document.querySelectorAll(
         "a,button,.btn-primary,.btn-secondary,.project-card,.service-card,.stat-box,.tech-card,.menu-toggle"
